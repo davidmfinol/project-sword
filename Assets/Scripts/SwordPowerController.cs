26b701e5a4e18c4b3daf73281f7de820e31d1f40
David Finol <davidmfinol@gmail.com>
2019-11-17 02:19:14 -0600

    Update teleportation

---
+++ Assets/Scripts/SwordPowerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SwordPowerController : MonoBehaviour
+{
+    public GameObject leftHand;
+    public GameObject leftSword;
+    public GameObject rightHand;
+    public GameObject rightSword;
+
+    public bool LeftPower => Input.GetButton("Left Power");
+    public bool LeftSwordGrip => Input.GetButton("Left SwordGrip");
+    public bool RightPower => Input.GetButton("Right Power");
+    public bool RightSwordGrip => Input.GetButton("Right SwordGrip");
+
+    void Update()
+    {
+        leftSword.SetActive(LeftSwordGrip);
+        rightSword.SetActive(RightSwordGrip);
+    }
+}
