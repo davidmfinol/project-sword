881c92a0765656399699d742e4b2394572289aa0
David Finol <davidmfinol@gmail.com>
2019-11-16 20:21:14 -0600

    Working on Sword power

--- Assets/Scripts/SwordPowerManager.cs
+++ Assets/Scripts/SwordPowerManager.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwordManager : MonoBehaviour
+public class SwordPowerManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
