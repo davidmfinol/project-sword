07004006996a058cde393dacd6a4d6c12586be80
David Finol <davidmfinol@gmail.com>
2019-11-17 17:47:03 -0600

    Switch teleportation and grip

---
+++ Assets/Scripts/NotBoolean.cs
@@ -0,0 +1,21 @@
+﻿namespace Zinnia.Data.Type.Transformation.Conversion
+{
+    using UnityEngine.Events;
+    using System;
+
+    public class NotBoolean : Transformer<bool, bool, NotBoolean.UnityEvent>
+    {
+        /// <summary>
+        /// Defines the event with the transformed <see cref="bool"/> value.
+        /// </summary>
+        [Serializable]
+        public class UnityEvent : UnityEvent<bool>
+        {
+        }
+
+        protected override bool Process(bool input)
+        {
+            return !input;
+        }
+    }
+}
