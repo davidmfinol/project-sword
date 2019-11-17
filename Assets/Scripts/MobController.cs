d392157b4a6971f50120b388c6595f41c0bc950b
Tony Nguyen <blue28275@yahoo.com>
2019-11-16 19:09:19 -0600

    added player controller and navmesh setup

--- Assets/Scripts/MobController.cs
+++ Assets/Scripts/MobController.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MobController : MonoBehaviour
 {
     private MobData mobData;
+    private NavMeshAgent navMeshAgent;
 
     void Awake()
     {
         mobData = GetComponent<Mob>().mobData;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        navMeshAgent.speed = mobData.moveSpeed;
     }
 
     void Follow(Transform target) { }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 [RequireComponent(typeof(HealthModule))]
+[RequireComponent(typeof(PlayerController))]
 public class Player : MonoBehaviour
 {
     private HealthModule healthModule;
